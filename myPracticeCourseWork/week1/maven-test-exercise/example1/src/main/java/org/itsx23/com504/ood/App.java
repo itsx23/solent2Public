4a0ebcf24ce84d5ca06ef899a5c43df966636257
Aeryk Del Mundo <4delma06@solent.ac.uk>
2019-09-25 12:53:45 +0100

    week1work

---
+++ myPracticeCourseWork/week1/maven-test-exercise/example1/src/main/java/org/itsx23/com504/ood/App.java
@@ -0,0 +1,13 @@
+package org.itsx23.com504.ood;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
